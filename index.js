5beac8bf485c8619cae5be15a27c5421b60acf3c
Kay <kyv.hardeman@gmail.com>
2021-09-29 08:50:17 -0400

    scrape: headlines from cnn

--- index.js
+++ index.js
@@ -4,5 +4,19 @@ const express = require('express')
 const PORT = 2020
 
 const app = express()
+const url = 'https://www.cnn.com/'
+
+axios(url)
+  .then( response => {
+    const html = response.data
+    const siteData = cheerio.load(html)
+
+    headlines('.cd__headline', html).each(function() {
+      const headlines = siteData(this).text()
+      const url = siteData(this).find('a').attr('href')
+    })
+
+
+  })
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT} `))
